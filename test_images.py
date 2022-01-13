51f9fa98e2250f0075c6da435d96c2074983b47b
oliverw10 <oliver@warrickfamily.com>
2022-01-13 11:57:32 +1100

    unpacked dist and angle

--- test_images.py
+++ test_images.py
@@ -5,21 +5,24 @@ from vision import Vision
 from camera_manager import MockCameraManager
 
 # load expected values for sample images
-# should be in format: image name,[...expected values]
+# should be in format: image name,dist,angle
 with open("test_images/expected.csv") as f:
     lines = f.read().split("\n")
     lines_values = [line.split(",") for line in lines]
-    # put expected output values into a tuple and convert to float
-    images = [( val[0], tuple(map(float, val[1:])) ) for val in lines_values]
+    # convert dist and angle to float
+    images = [( val[0], float(val[1]), float(val[2]) ) for val in lines_values]
 
 # TODO: could calculate the allowed error based on what would result in a shot missing 
 allowed_azimuth_error = math.radians(5) 
 allowed_dist_error = 0.3
 
-@pytest.mark.parametrize("filename,expected", images)
-def test_sample_images(filename: str, expected: Tuple[float, float]):
+@pytest.mark.parametrize("filename,expected_dist,expected_angle", images)
+def test_sample_images(filename: str, expected_dist: float, expected_angle: float):
     vision = Vision(MockCameraManager(filename))
-    outputs = (41.99, 1.9)#vision.run()
-    error = [abs(output-expected) for output, expected in zip(outputs, expected)]
-    assert error[0] < allowed_azimuth_error
-    assert error[1] < allowed_dist_error
+    output_dist, output_angle = (41.99, 1.99)#vision.run()
+
+    dist_error = abs(output_dist-expected_dist)
+    azimuth_error = abs(output_angle-expected_angle)
+
+    assert dist_error < allowed_dist_error
+    assert azimuth_error < allowed_azimuth_error
