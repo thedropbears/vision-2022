8e7aa0cac431a20d89e501c2cb7480c40b1af218
David Vo <auscompgeek@users.noreply.github.com>
2022-01-29 16:38:21 +1100

    Isolate expected failing tests (#12)

--- test_images.py
+++ test_images.py
@@ -1,17 +1,19 @@
+import csv
 import pytest
 import cv2
 import vision
 
-# load expected values for sample images
-# should be in format: image name,dist,angle
-with open("test_images/expected.csv") as f:
-    lines = f.read().split("\n")
-    lines_values = [line.split(",") for line in lines]
-    print(lines_values)
-    # convert dist and angle to float
-    images = [
-        (val[0], float(val[1]), float(val[2])) for val in lines_values if len(val) == 3
-    ]
+
+def read_test_data_csv(fname: str):
+    with open(fname) as f:
+        result = []
+        for image, x, y in csv.reader(f):
+            result.append((image, float(x), float(y)))
+    return result
+
+
+images = read_test_data_csv("test_images/expected.csv")
+fail_images = read_test_data_csv("test_images/expected_fail.csv")
 
 # TODO: could calculate the allowed error based on what would result in a shot missing
 allowed_x_error = 0.1
@@ -21,7 +23,24 @@ allowed_y_error = 0.1
 @pytest.mark.parametrize("filename,expected_x,expected_y", images)
 def test_sample_images(filename: str, expected_x: float, expected_y: float):
     image = cv2.imread(f"./test_images/other/{filename}")
-    assert not image is None
+    assert image is not None
+    results, _ = vision.process_image(image)
+
+    assert results is not None
+    output_x, output_y = results
+
+    x_error = abs(output_x - expected_x)
+    y_error = abs(output_y - expected_y)
+
+    assert x_error < allowed_x_error
+    assert y_error < allowed_y_error
+
+
+@pytest.mark.parametrize("filename,expected_x,expected_y", fail_images)
+@pytest.mark.xfail
+def test_sample_images_fail(filename: str, expected_x: float, expected_y: float):
+    image = cv2.imread(f"./test_images/other/{filename}")
+    assert image is not None
     results, _ = vision.process_image(image)
 
     assert results is not None
